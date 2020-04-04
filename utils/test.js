4bba6246af2d572b39c2e3a2d98f003371397d3c
Felix <lang.felix@posteo.net>
2020-04-04 17:24:33 +0200

    file delete

--- utils/test.js
+++
@@ -1,7 +0,0 @@
-return db.query(
-    `INSERT INTO users (first, last, email, userID) VALUES
-    ($1, $2, $3, $4)
-    ON CONFLICT (userID)
-    DO UPDATE SET first = ($1), last = ($2), email = ($3)`,
-    [first || null, last || null, email || null, userID]
-);
